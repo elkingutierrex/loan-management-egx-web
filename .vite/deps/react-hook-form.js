746af4812188db128a89e186e072c3a6caa59865
elkingutierrex <elkin.gutierrex@gmail.com>
2026-06-22 10:54:55 -0500

    chore: enforce updated .gitignore rules

---
+++ .vite/deps/react-hook-form.js
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  Form,
+  FormProvider,
+  FormStateSubscribe,
+  Watch,
+  appendErrors,
+  createFormControl,
+  get,
+  set,
+  useController,
+  useFieldArray,
+  useForm,
+  useFormContext,
+  useFormState,
+  useWatch
+} from "./chunk-MT535BFG.js";
+import "./chunk-TBKXTGHY.js";
+import "./chunk-4MBMRILA.js";
+export {
+  Controller,
+  Form,
+  FormProvider,
+  FormStateSubscribe,
+  Watch,
+  appendErrors,
+  createFormControl,
+  get,
+  set,
+  useController,
+  useFieldArray,
+  useForm,
+  useFormContext,
+  useFormState,
+  useWatch
+};
